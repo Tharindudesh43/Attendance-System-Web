34493385a0c73da1dca877d8fa69557f81dc41b7
Tharindudesh43 <tharindudeshanhimahansa43@gmail.com>
2026-07-12 01:51:01 +0530

    Frontend update

--- src/pages/subjectsdetails.jsx
+++ src/pages/subjectsdetails.jsx
@@ -43,138 +43,142 @@ const SubjectsDetails = () => {
       }
     };
     fetchSubjects();
-  }, [reload]);
+  }, [reload, id]);
 
   return (
-    <div class="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-50/30">
       <Header />
-      <div className="relative w-full h-auto p-4">
+      
+      <div className="w-full px-4 pt-6 pb-2 md:px-8">
         <button
           onClick={() => navigateback()}
-          className="absolute top-0 left-0 flex items-center justify-center px-4 py-2 m-4 text-black bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:outline-none"
+          className="inline-flex items-center justify-center px-4 py-2 text-sm text-black transition-colors bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
         >
-          <div className="grid items-center justify-center grid-cols-2">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                style={{ marginRight: "8px" }}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15 8a.5.5 0 0 1-.5.5H2.707l4.147 4.146a.5.5 0 0 1-.708.708l-5-5a.5.5 0 0 1 0-.708l5-5a.5.5 0 1 1 .708.708L2.707 7.5H14.5a.5.5 0 0 1 .5.5z"
-                />
-              </svg>
-            </div>
-            <div className="text-sm">Back</div>
-          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            className="mr-2"
+          >
+            <path
+              fillRule="evenodd"
+              d="M15 8a.5.5 0 0 1-.5.5H2.707l4.147 4.146a.5.5 0 0 1-.708.708l-5-5a.5.5 0 0 1 0-.708l5-5a.5.5 0 1 1 .708.708L2.707 7.5H14.5a.5.5 0 0 1 .5.5z"
+            />
+          </svg>
+          Back
         </button>
       </div>
 
-      <div className="flex flex-grow grid-cols-2 gap-10 pt-10 pl-5 pr-4 md:grid-cols-2">
-        <div class="overflow-x-auto w-full">
-          <table class="min-w-full bg-white">
-            <thead class="bg-gray-100 whitespace-nowrap">
-              <tr>
-                <th class="p-4 text-left text-[13px] font-semibold text-slate-900">
-                  Academic Year
-                </th>
-                <th class="p-4 text-left text-[13px] font-semibold text-slate-900">
-                  Year
-                </th>
-                <th class="p-4 text-left text-[13px] font-semibold text-slate-900">
-                  Semester
-                </th>
-                <th class="p-4 text-left text-[13px] font-semibold text-slate-900">
-                  Subject
-                </th>
-                <th class="p-4 text-left text-[13px] font-semibold text-slate-900">
-                  Sem Version
-                </th>
-                <th class="p-4 text-left text-[13px] font-semibold text-slate-900">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            {loadingimg2 ? (
-              <td colspan="5" class="p-4  font-medium text-center">
-                <div className="flex items-center justify-center w-full h-30 col-span-full">
-                  <img
-                    src={loadingimage}
-                    alt="Loading..."
-                    className="w-25 h-25"
-                  />
-                </div>
-              </td>
-            ) : allsubjects && allsubjects == "No subjects available" ? (
-              <tbody class="whitespace-nowrap">
-                <tr class="hover:bg-gray-50">
-                  <td
-                    colspan="5"
-                    class="p-4 text-[15px] text-slate-900 font-medium text-center"
-                  >
-                    No Data Added Yet.
-                  </td>
+      <div className="flex flex-col flex-grow w-full px-4 pt-4 pb-10 mx-auto md:px-8 max-w-7xl">
+        <div className="w-full overflow-hidden bg-white border border-gray-200 shadow-sm rounded-xl">
+          <div className="w-full overflow-x-auto">
+            <table className="min-w-full bg-white whitespace-nowrap">
+              <thead className="bg-gray-100 border-b border-gray-200">
+                <tr>
+                  <th className="p-4 text-left text-[13px] font-semibold text-slate-900">
+                    Academic Year
+                  </th>
+                  <th className="p-4 text-left text-[13px] font-semibold text-slate-900">
+                    Year
+                  </th>
+                  <th className="p-4 text-left text-[13px] font-semibold text-slate-900">
+                    Semester
+                  </th>
+                  <th className="p-4 text-left text-[13px] font-semibold text-slate-900">
+                    Subject
+                  </th>
+                  <th className="p-4 text-left text-[13px] font-semibold text-slate-900">
+                    Sem Version
+                  </th>
+                  <th className="p-4 text-left text-[13px] font-semibold text-slate-900">
+                    Actions
+                  </th>
                 </tr>
-              </tbody>
-            ) : (
-              <tbody class="whitespace-nowrap">
-                {allsubjects.map((subjectSet, index) =>
-                  subjectSet.map((subjectDetails, subIndex) => (
-                    <tr
-                      key={`${index}-${subIndex}`}
-                      className="hover:bg-gray-50"
+              </thead>
+              
+              {loadingimg2 ? (
+                <tbody>
+                  <tr>
+                    <td colSpan={6} className="p-8 text-center">
+                      <div className="flex items-center justify-center w-full">
+                        <img
+                          src={loadingimage}
+                          alt="Loading..."
+                          className="w-16 h-16 md:w-20 md:h-20"
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              ) : allsubjects && allsubjects === "No subjects available" ? (
+                <tbody>
+                  <tr className="hover:bg-gray-50">
+                    <td
+                      colSpan={6}
+                      className="p-8 text-[15px] text-slate-500 font-medium text-center"
                     >
-                      {/* Displaying the subject details */}
-                      {subjectDetails.map((item, itemIndex) => (
-                        <td
-                          key={`${subIndex}-${itemIndex}`}
-                          className="p-4 text-[15px] text-slate-900 font-medium"
-                        >
-                          {item}
-                        </td>
-                      ))}
-                      <td class="p-4">
-                        <div class="flex items-center justify-content-center">
-                          <button
-                            title="Delete"
-                            className="cursor-pointer"
-                            onClick={() => {
-                              setyear(subjectDetails[1]);
-                              setsubject(subjectDetails[3]);
-                              setidpassed(id);
-                              setsubjectIndex(index);
-                              setDeleteConfirmShowModal(true);
-                            }}
+                      No Data Added Yet.
+                    </td>
+                  </tr>
+                </tbody>
+              ) : (
+                <tbody className="divide-y divide-gray-100">
+                  {allsubjects.map((subjectSet, index) =>
+                    subjectSet.map((subjectDetails, subIndex) => (
+                      <tr
+                        key={`${index}-${subIndex}`}
+                        className="transition-colors hover:bg-gray-50"
+                      >
+                        {subjectDetails.map((item, itemIndex) => (
+                          <td
+                            key={`${subIndex}-${itemIndex}`}
+                            className="p-4 text-[14px] md:text-[15px] text-slate-800 font-medium"
                           >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="w-5 h-5 fill-red-500 hover:fill-red-700"
-                              viewBox="0 0 24 24"
+                            {item}
+                          </td>
+                        ))}
+                        <td className="p-4">
+                          <div className="flex items-center justify-start">
+                            <button
+                              title="Delete"
+                              className="p-2 transition-colors rounded-lg cursor-pointer hover:bg-red-50"
+                              onClick={() => {
+                                setyear(subjectDetails[1]);
+                                setsubject(subjectDetails[3]);
+                                setidpassed(id);
+                                setsubjectIndex(index);
+                                setDeleteConfirmShowModal(true);
+                              }}
                             >
-                              <path
-                                d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z"
-                                data-original="#000000"
-                              />
-                              <path
-                                d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z"
-                                data-original="#000000"
-                              />
-                            </svg>
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-                  )),
-                )}
-              </tbody>
-            )}
-          </table>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-5 h-5 transition-colors fill-red-500 hover:fill-red-700"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z"
+                                  data-original="#000000"
+                                />
+                                <path
+                                  d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z"
+                                  data-original="#000000"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    )),
+                  )}
+                </tbody>
+              )}
+            </table>
+          </div>
         </div>
       </div>
+      
       {DeleteConfirmShowModal && (
         <DeleteSubjectConfirmModal
           id={id}
